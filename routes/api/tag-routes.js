bb73f0de35d625fb8590483b545e89dd4d89be2e
Mike Gonzalez <Mike.Gon1217@gmail.com>
2022-08-13 21:48:25 -0400

    adding the README.md

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,82 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  try{
+    const tagsData = await Tag.findAll({
+      include: {model:Product, through:ProductTag},
+    // });
+  })
+  res.status(200).json(tagsData);
+}  catch(err){
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  try{
+    const tagsData = await Tag.findByPk(req.params.id,{
+      include: Product
+    })
+    res.status(200).json(tagsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new tag
+  try{
+    const tagsData = await Tag.create(req.body);
+    res.status(200).json(tagsData);
+  } 
+  catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a tag's name by its `id` value
+  try{
+    const tagsData = Tag.update(req.body,{
+      where:{
+        id: req.params.id,
+      }
+    });
+    if (!tagsData){
+      res.status(404).json({ message: "No location found with this id"})
+      return;
+    }
+    res.status(200).json(tagsData);
+  } 
+  catch (err){
+    res.status(500).json(err);
+  }
+});
+
+
+router.delete('/:id', async (req, res) => {
+  // delete on tag by its `id` value
+  try{
+    const tagsData = await Tag.destroy({
+      where:{
+        id: req.params.id,
+      }
+    });
+    if (!tagsData){
+      res.status(404).json({ message: "No location found with this id"})
+      return;
+    }
+    res.status(200).json(tagsData);
+  } catch (err){
+res.status(500).json(err);
+  }
+});
+
+module.exports = router;
